0a04d061ec4d128898a6e20d9a2a281fad7bca64
Anmol Jain <anmolj678@gmail.com>
2023-05-21 04:17:48 -0700

    allow localhost

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.errors.handlers import http_exception_handler
 from app.api import api_router as router
 
@@ -10,3 +11,15 @@ app = FastAPI()
 app.add_exception_handler(HTTPException, http_exception_handler)
 
 app.include_router(router, prefix="/api")
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
