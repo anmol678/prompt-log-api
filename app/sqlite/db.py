fe68eeab347d6eaf60936ba83c1ff641fa230bdb
Anmol Jain <anmolj678@gmail.com>
2023-06-21 23:55:32 -0700

    add db checks

--- app/sqlite/db.py
+++ app/sqlite/db.py
@@ -3,7 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from dotenv import load_dotenv
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise RuntimeError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
