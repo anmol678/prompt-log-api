a061a1a3d6bd416e7c6430879d6a39764ac4f124
Anmol Jain <anmolj678@gmail.com>
2023-07-25 17:18:04 +0530

    update websocket manager

--- app/api/websocket_manager.py
+++ app/api/websocket_manager.py
@@ -1,9 +1,11 @@
 from fastapi import WebSocket
+from app.sqlite.schemas import Log
 
 
 class ConnectionManager:
     def __init__(self):
         self.active_connections: dict[str, WebSocket] = {}
+        self.new_logs: list[Log] = []
 
     async def connect(self, client_id: str, websocket: WebSocket):
         await websocket.accept()
@@ -15,5 +17,14 @@ class ConnectionManager:
 
     async def send_message(self, client_id: str, message: str):
         await self.active_connections[client_id].send_text(message)
+    
+    def add_log(self, log: Log):
+        self.new_logs.append(log)
+
+    async def send_notification(self):
+        self.new_logs.pop()
+        for client_id in self.active_connections.keys():
+            await self.send_message(client_id, "new logs")
+
 
 manager = ConnectionManager()
